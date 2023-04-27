0709d7573ea6e6ea14f59af7725719280df7fa43
Mehmet Tarhan <hi@memtarhan.com>
2023-04-27 19:39:39 +0100

    [Apple SwiftUI] - Customise the Row Preview

--- AppleTutorials/Landmarks/Landmarks/Views/LandmarkRow.swift
+++ AppleTutorials/Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -15,8 +15,9 @@ struct LandmarkRow: View {
             landmark.image
                 .resizable()
                 .frame(width: 50, height: 50)
-            Text(landmark.name)
             
+            Text(landmark.name)
+
             Spacer()
         }
     }
@@ -24,6 +25,12 @@ struct LandmarkRow: View {
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[0])
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+
     }
 }
