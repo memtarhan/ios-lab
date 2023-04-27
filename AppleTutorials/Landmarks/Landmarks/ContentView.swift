00a03f63bdb3e75a45e578352d929883a982b5cf
Mehmet Tarhan <hi@memtarhan.com>
2023-04-27 17:43:46 +0100

    [Apple SwiftUI] - Texts

---
+++ AppleTutorials/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Mehmet Tarhan on 27/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            .fontWeight(.heavy)
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AppleTutorials/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mehmet Tarhan on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
