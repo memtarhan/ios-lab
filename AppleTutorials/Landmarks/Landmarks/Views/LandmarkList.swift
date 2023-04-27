78da7bf0d488f77213ff41f8463fd6346beb980c
Mehmet Tarhan <hi@memtarhan.com>
2023-04-28 00:02:18 +0100

    [Apple SwiftUI] - Add a Control to Toggle the State

--- AppleTutorials/Landmarks/Landmarks/Views/LandmarkList.swift
+++ AppleTutorials/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -9,19 +9,28 @@ import SwiftUI
 
 struct LandmarkList: View {
     @State private var showFavoritesOnly = false
-    
+
     var filteredLandmarks: [Landmark] {
         landmarks.filter { landmark in
-            (!showFavoritesOnly || landmark.isFavorite)
+            !showFavoritesOnly || landmark.isFavorite
         }
     }
+
     var body: some View {
         NavigationStack {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                /*
+                 To combine static and dynamic views in a list, or to combine two or more different groups of dynamic views, use the ForEach type instead of passing your collection of data to List.
+                 */
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .listStyle(.plain)
