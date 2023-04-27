7947d84e600a635b64cab8833f6410debe791964
Mehmet Tarhan <hi@memtarhan.com>
2023-04-27 23:59:37 +0100

    [Apple SwiftUI] - Filter the List View

--- AppleTutorials/Landmarks/Landmarks/Views/LandmarkList.swift
+++ AppleTutorials/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     var body: some View {
         NavigationStack {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
