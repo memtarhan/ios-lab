9780eb0740b4d675c63a715b9e9c150829841e29
Mehmet Tarhan <hi@memtarhan.com>
2023-04-29 02:54:44 +0100

    [Apple SwiftUI] - Compose Animations for Complex Effects

--- AppleTutorials/Landmarks/Landmarks/Views/Hikes/HikeGraph.swift
+++ AppleTutorials/Landmarks/Landmarks/Views/Hikes/HikeGraph.swift
@@ -7,6 +7,14 @@ The elevation, heart rate, and pace of a hike plotted on a graph.
 
 import SwiftUI
 
+extension Animation {
+    static func ripple(index: Int) -> Animation {
+        Animation.spring(dampingFraction: 0.5)
+            .speed(2)
+            .delay(0.03 * Double(index))
+    }
+}
+
 struct HikeGraph: View {
     var hike: Hike
     var path: KeyPath<Hike.Observation, Range<Double>>
@@ -40,6 +48,7 @@ struct HikeGraph: View {
                         range: observation[keyPath: path],
                         overallRange: overallRange
                     )
+                    .animation(.ripple(index: index))
                 }
                 .offset(x: 0, y: proxy.size.height * heightRatio)
             }
@@ -49,10 +58,10 @@ struct HikeGraph: View {
 
 func rangeOfRanges<C: Collection>(_ ranges: C) -> Range<Double>
     where C.Element == Range<Double> {
-    guard !ranges.isEmpty else { return 0..<0 }
+    guard !ranges.isEmpty else { return 0 ..< 0 }
     let low = ranges.lazy.map { $0.lowerBound }.min()!
     let high = ranges.lazy.map { $0.upperBound }.max()!
-    return low..<high
+    return low ..< high
 }
 
 func magnitude(of range: Range<Double>) -> Double {
