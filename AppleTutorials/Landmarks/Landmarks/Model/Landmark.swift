da02f832eb024a0c04f2472173ef1051b4bbff39
Mehmet Tarhan <hi@memtarhan.com>
2023-04-29 13:21:47 +0100

    [Apple SwiftUI] - Complete the Category View

--- AppleTutorials/Landmarks/Landmarks/Model/Landmark.swift
+++ AppleTutorials/Landmarks/Landmarks/Model/Landmark.swift
@@ -20,6 +20,7 @@ struct Landmark: Hashable, Codable, Identifiable {
     var state: String
     var description: String
     var isFavorite: Bool
+    var isFeatured: Bool
 
     var category: Category
     enum Category: String, CaseIterable, Codable {
