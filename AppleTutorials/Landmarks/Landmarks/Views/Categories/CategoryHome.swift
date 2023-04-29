912f7fd09fe4fcd5c2b72464b8e8c147d0bb8cf0
Mehmet Tarhan <hi@memtarhan.com>
2023-04-29 03:00:42 +0100

    [Apple SwiftUI] - Add a Category View

---
+++ AppleTutorials/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Mehmet Tarhan on 29/04/2023.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
