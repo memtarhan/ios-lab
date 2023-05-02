675cdc61ba624a73c9d29e2256dcd7d39904d9e7
Mehmet Tarhan <hi@memtarhan.com>
2023-05-02 14:24:37 +0100

    [SwiftUI by Example] - Using Shapes

--- SwiftUI-by-Example/Chapter-3/Images/Images/ContentView.swift
+++ SwiftUI-by-Example/Chapter-3/Images/Images/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
             Gradients()
             Divider()
             ImageBackground()
+            Divider()
+            Shapes()
         }
     }
 }
@@ -111,6 +113,33 @@ struct ImageBackground: View {
     }
 }
 
+
+struct Shapes: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+                .fill(.red)
+                .frame(width: 200, height: 200)
+            
+            Circle()
+                .fill(.blue)
+                .frame(width: 100, height: 100)
+            
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.green)
+                .frame(width: 150, height: 100)
+            
+            Capsule()
+                .fill(.green)
+                .frame(width: 150, height: 100)
+            
+            Circle()
+                .strokeBorder(.black, lineWidth: 20)
+                .background(Circle().fill(.blue))
+                .frame(width: 150, height: 150)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
