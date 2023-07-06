381c7b78dbaa4a30f0fbf57b28098b0daf1e291f
Mehmet Tarhan <hi@memtarhan.com>
2023-07-06 23:31:12 +0300

    [compositional-layout] - lab

---
+++ Compositonal-Layout/Ecommerce/Ecommerce/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Ecommerce
+//
+//  Created by Mehmet Tarhan on 06/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+
+
+}
+
