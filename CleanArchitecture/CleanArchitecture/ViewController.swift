38ec49cfc96ed72c0b8d115e123a8eecfcad00b2
Mehmet Tarhan <hi@memtarhan.com>
2023-11-24 00:18:40 +0000

    [Clean Architecture] - created sample project

---
+++ CleanArchitecture/CleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by Mehmet Tarhan on 24/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
